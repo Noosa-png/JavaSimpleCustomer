f2cc1205e481e627293d450cdcecabbc8135d840
Noosa-png <67515425+Noosa-png@users.noreply.github.com>
2021-04-20 13:04:11 +0100

    Update ReadDelimitedFile.java

--- src/main/java/ReadDelimitedFile.java
+++ src/main/java/ReadDelimitedFile.java
@@ -30,3 +30,93 @@ public class ReadDelimitedFile {
         return fileData;
     }
 }
+package Fake;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LoadDelimitedFile {
+
+    private String fileDelimitor = ",";
+
+    private Load loadFile = new LoadFile();
+
+    public void setFileDelimitor(String fileDelimitor){
+        this.fileDelimitor = fileDelimitor;
+    }
+    
+    package Stub;
+
+import Foundation.LoadFile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LoadDelimitedFile {
+
+    private String fileDelimitor = ",";
+
+    private Stub.Load loadFile = new Stub.LoadFile();
+
+    public void setFileDelimitor(String fileDelimitor){
+        this.fileDelimitor = fileDelimitor;
+    }
+
+    public void setLoadFile(Load loadFile) {
+        this.loadFile = loadFile;
+    }
+
+    public List<String[]> getFileData(String fileName) {
+        List<String[]> delimtedData = new ArrayList<String[]>();
+
+        List<String> fileData = loadFile.getFileData(fileName);
+        for (String row : fileData) {
+            delimtedData.add(row.split(fileDelimitor));
+        }
+        return delimtedData;
+    }
+}
+
+    public void setLoadFile(Load loadFile) {
+        this.loadFile = loadFile;
+    }
+
+    public List<String[]> getFileData(String fileName) {
+        List<String[]> delimtedData = new ArrayList<String[]>();
+
+        List<String> fileData = loadFile.getFileData(fileName);
+        for (String row : fileData) {
+            delimtedData.add(row.split(fileDelimitor));
+        }
+        return delimtedData;
+    }
+}
+package Mock;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LoadDelimitedFile {
+
+    private String fileDelimitor = ",";
+
+    private Load loadFile = new LoadFile();
+
+    public void setFileDelimitor(String fileDelimitor){
+        this.fileDelimitor = fileDelimitor;
+    }
+
+    public void setLoadFile(Load loadFile) {
+        this.loadFile = loadFile;
+    }
+
+    public List<String[]> getFileData(String fileName) {
+        List<String[]> delimtedData = new ArrayList<String[]>();
+
+        List<String> fileData = loadFile.getFileData(fileName);
+        for (String row : fileData) {
+            delimtedData.add(row.split(fileDelimitor));
+        }
+        return delimtedData;
+    }
+}
